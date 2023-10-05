e0b4b9afa855bdf8260e2fc55b8b46e9933c050c
Nguyen Linh 2002 <nguyeenlinh82002@gmail.com>
2023-10-05 15:15:47 +0700

    init user

---
+++ MQ_Shop.WebApp/Models/user.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MQ_Shop.WebApp.Models
+{
+    public class user
+    {
+        int id { get; set; }
+    }
+}
